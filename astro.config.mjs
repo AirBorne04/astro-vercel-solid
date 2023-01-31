86fa60b3e985d73d11042cc5b4f9aca2f13f9e8f
AirBorne04 <>
2023-01-31 12:21:51 +0100

    no solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
